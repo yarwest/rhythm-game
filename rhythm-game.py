fb81d0c34b785cad37de877eead9cd7181e40672
yarwest <yarnoboelens@gmail.com>
2017-07-07 00:47:49 +0200

    Added player movement across the lines and let the game create objects on these lines. The objects move and whenever they reach a certain point the game checks if the player is on the right line and deletes it. If the object reaches the end of the line the game is/should be over.

--- rhythm-game.py
+++ rhythm-game.py
@@ -1,13 +1,47 @@
 # Functions
+from random import randrange
+linesNo = 4
+yOffset = 175
+lineHeight = 2
+lineOffset = 50
 
 def drawLines():
     for i in range(linesNo):
-        y = 175 + (50 * i)
-        pygame.draw.rect(screen, (255,255,255), pygame.Rect(0, y, 750, 2))
+        y = (yOffset-(lineHeight/2)) + (lineOffset * i)
+        pygame.draw.rect(screen, (255,255,255), pygame.Rect(0, y, 750, lineHeight))
+
+playerHeight = 20
+playerWidth = 4
+playerOffset = 50
 
 def drawPlayer(lineNumber):
-    y = 174 + (50 * lineNumber)
-    pygame.draw.rect(screen, (255,0,0), pygame.Rect(23,y,4,4))
+    y = (yOffset-(playerHeight/2)) + (lineOffset * lineNumber)
+    pygame.draw.rect(screen, (255,0,0), pygame.Rect((playerOffset-(playerWidth/2)),y,playerWidth,playerHeight))
+
+objects = [
+]
+
+colors = [
+    (0,0,255),
+    (0,255,0),
+    (255,0,255),
+    (255,255,0)
+]
+
+def updateObjects():
+    if objects:
+        for object in objects:
+            object[0] = object[0]-3
+            if object[0] <= playerOffset:
+                if playerPosition == object[1]:
+                    objects.remove(object)
+            elif object[0] <= 0:
+                print "game over"
+            y = (yOffset-(playerHeight/2)) + (lineOffset * object[1])
+            pygame.draw.rect(screen, colors[object[1]], pygame.Rect(object[0],y,playerHeight,playerHeight))
+    else:
+        print "no objects"
+        objects.append([750,randrange(0, 4, 1)])
 
 # The actual game
 import pygame
@@ -16,18 +50,29 @@ pygame.init()
 screen = pygame.display.set_mode((750,500)#, pygame.FULLSCREEN, 16
     )
 
-linesNo = 4
 playerPosition = 0
+clock = pygame.time.Clock()
 done = False
 
 while not done:
+    clock.tick(60)
     screen.fill((0,0,0))
     drawLines()
     drawPlayer(playerPosition)
+    updateObjects()
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerPosition = 0
+            elif event.key == pygame.K_UP:
+                playerPosition = 1
+            elif event.key == pygame.K_RIGHT:
+                playerPosition = 2
+            elif event.key == pygame.K_DOWN:
+                playerPosition = 3
         pygame.display.flip()
 
 pygame.display.quit()
